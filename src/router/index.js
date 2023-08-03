9f25fb1760d23ebbf925efe924e9330307cbfb67
actor <2507618302@qq.com>
2023-08-03 17:13:40 +0800

    fix: 删除test路由

--- src/router/index.js
+++ src/router/index.js
@@ -11,23 +11,11 @@ const routes = [
     name: 'Demo',
     component: () => import('views/demo/index.vue'),
   },
-  {
-    path: '/test',
-    name: 'Test',
-    component: () => import('views/test/index.vue'),
-  },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  scrollBehavior() {
-    return {
-      el: '#app',
-      top: 0,
-      behavior: 'smooth',
-    };
-  },
 });
 
 router.beforeEach(beforeEach);
